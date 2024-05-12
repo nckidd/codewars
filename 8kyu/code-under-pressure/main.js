efe1581fa1de6213da12f529417e7ea354c8d0a7
nchristene <christenekidd@gmail.com>
2024-05-12 11:52:08 -0800

    add practice

---
+++ 8kyu/code-under-pressure/main.js
@@ -0,0 +1,14 @@
+function doubleInteger(i) {
+    // return doubled
+    return i*2;
+  }
+  
+  /*
+  -p: int
+  -r: int
+  -e: i=2 -> 4
+  -pseudo: ^
+  */
+
+ console.log(doubleInteger(2)) //returns 4
+ console.log(doubleInteger(4)) //returns 8
