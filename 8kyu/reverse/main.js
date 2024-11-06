cbd320ffafead7aa04387b2f20860a72519649ae
nchristene <christenekidd@gmail.com>
2024-11-05 19:39:01 -0800

    add practice

---
+++ 8kyu/reverse/main.js
@@ -0,0 +1,10 @@
+function reverseList(list) {
+    //return reversed arr
+    return list.reverse()
+  }
+  /*
+  --param: array of ints
+  --returns: reversed arr
+  --example: <
+  -- pseudo: ^
+  */
