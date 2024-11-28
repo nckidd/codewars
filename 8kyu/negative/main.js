c327a659b252938d19d49614b4a18e6c30d00977
nchristene <christenekidd@gmail.com>
2024-11-27 16:45:00 -0800

    add practice

---
+++ 8kyu/negative/main.js
@@ -0,0 +1,13 @@
+function makeNegative(num) {
+    //return neg of abs
+    return -Math.abs(num)
+  }
+  /*
+  -p: int
+  -r: negative int if non-zero
+  -e: <
+  -p: ^
+  */
+
+  console.log(makeNegative(-8)) //returns -8
+  console.log(makeNegative(0)) //returns 0
