81036b2aa0b33f39b9d7ba94f00825ad4979d1cc
nchristene <christenekidd@gmail.com>
2024-12-03 18:38:00 -0800

    add practice

---
+++ 8kyu/return-str/main.js
@@ -0,0 +1,12 @@
+function greet(name){
+    //return str temp literal 
+    return `Hello, ${name} how are you doing today?`
+  }
+  /*
+  -p: name
+  -r: string incl name
+  -e: greet("ally") -> "Hello, Ally how are you doing today?"
+  -p: ^
+  */
+
+  console.log(greet('Ally')) // returns "Hello, Ally how are you doing today?"
